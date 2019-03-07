35f83c5f1c0e3e7307b32476c66827497e836514
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-03-07 19:32:43 +0100

    add argument

--- src/main.py
+++ src/main.py
@@ -3,10 +3,25 @@ import parse
 from models import *
 
 def main():
-	group = 'INFOS4A1-1'
-	weeks = parse.get_current_week(group)
-	for w in weeks:
-		print(w)
+	argp = argparse.ArgumentParser(description="A simple ichronos parser write in python3", \
+	epilog="By Cédric FARINAZZO <cedric.farinazzo@gmail.com>\n")
+	
+	argp.add_argument("group", help=" Group name of your class (ex: INFOS4A1-1)", type=str)
+	argp.add_argument("-v", "--verbosity", help="increase output verbosity", action="store_true")
+	argp.add_argument("-w", "--week", help="Week id", type=str)
+	argp.add_argument("--nocolor", help="Disable colored output", action="store_true")
+	
+	args = argp.parse_args()
+	#print(args)
+	
+	if args.week is None:
+		weeks = parse.get_current_week(args.group)
+		for w in weeks:
+			print(w)
+	else:
+		weeks = parse.get_custom_week(args.group, args.week)
+		for w in weeks:
+			print(w)
 	
 if __name__ == '__main__':
 	main()
