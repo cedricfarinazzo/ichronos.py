559c3940bbb172e740421ac3b8a3adc7d8510349
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-03-07 21:01:30 +0100

    remove whitespace

--- src/main.py
+++ src/main.py
@@ -7,15 +7,15 @@ from models import *
 def main():
 	argp = argparse.ArgumentParser(description="A simple ichronos parser write in python3", \
 	epilog="By Cédric FARINAZZO <cedric.farinazzo@gmail.com>\n")
-	
+
 	argp.add_argument("group", help=" Group name of your class (ex: INFOS4A1-1)", type=str)
 	argp.add_argument("-v", "--verbosity", help="increase output verbosity", action="store_true")
 	argp.add_argument("-w", "--week", help="Week id (see more here: https://ichronos.net/about", type=int)
 	argp.add_argument('--nocolor', dest='nocolor', default=False, action='store_true', help='Argument to disable colored output')
-	
+
 	args = argp.parse_args()
 	#print(args)
-	
+
 	if args.week is None:
 		weeks = parse.get_current_week(args.group, nocolor=args.nocolor)
 		for w in weeks:
